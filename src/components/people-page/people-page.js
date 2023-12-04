74a1d09dac6ac831e27a9d8d1b3ca42f41875448
sayora <sayora@gmail.com>
2023-12-05 02:18:59 +0600

    13 commit

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import './people-page.css';
 import ItemList from '../item-list';
-import PersonDetails from "../person-details";
-import ErrorIndicator from "../error-indicator";
+import ItemDetails from "../item-details";
 import SwapiService from "../../services/swapi-service";
 import Row from "../row";
-import ErrorButton from "../error-button";
 import ErrorBoundry from "../error-boundry";
+
 
 
 export default class PeoplePage extends Component {
@@ -14,9 +13,8 @@ export default class PeoplePage extends Component {
 
     state = {
         selectedPerson: 3,
-        hasError: false
-    };
 
+    };
 
 
     onPersonSelected = (selectedPerson) => {
@@ -24,22 +22,23 @@ export default class PeoplePage extends Component {
     };
 
     render() {
-        if(this.state.hasError) return <ErrorIndicator />;
-        const itemList = (<ItemList
-            onItemSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}
-        >
-            { (i) => `${i.name} (${i.birthYear})}` }
-        </ItemList>);
+        const itemList = (
+            <ItemList
+                onItemSelected={this.onPersonSelected}
+                getData={this.swapiService.getAllPeople}
+            >
+                { (i) => `${i.name} (${i.birthYear})` }
+            </ItemList>
+        );
 
-        const personDetails = (
-            <ErrorBoundry>
-            <PersonDetails personId={this.state.selectedPerson} />
+        const itemDetails = (
+            <ErrorBoundry>                
+                <ItemDetails itemId={this.state.selectedPerson} />
             </ErrorBoundry>
-            );
+        );
 
         return (
-                <Row left={itemList} right={personDetails} />
+            <Row left={itemList} right={itemDetails} />
         )
     }
 }
